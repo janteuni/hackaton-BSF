44032991504d9465be531a90cf555f80d2182fc4
Mélodie <melodie.sagodira@gmail.com>
2015-02-22 03:37:20 +0100

    minot style update

--- client/views/play/play.controller.js
+++ client/views/play/play.controller.js
@@ -21,27 +21,6 @@ angular.module('bsf')
       .catch(function (err) {
         console.dir(err.data);
       });
-
-    function insertAtCursor(myField, myValue) {
-      //IE support
-      if (document.selection) {
-        myField.focus();
-        var sel = document.selection.createRange();
-        sel.text = myValue;
-      }
-      //MOZILLA and others
-      else if (myField.selectionStart || myField.selectionStart == '0') {
-        var startPos = myField.selectionStart;
-        var endPos = myField.selectionEnd;
-        myField.value = myField.value.substring(0, startPos)
-        + myValue
-        + myField.value.substring(endPos, myField.value.length);
-        myField.selectionStart = startPos + myValue.length;
-        myField.selectionEnd = startPos + myValue.length;
-      } else {
-        myField.value += myValue;
-      }
-    }
     
     $scope.insert = function(str)  {
 
